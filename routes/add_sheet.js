dd8f49b0ae1e23bd295d9223b42ece1b148436c3
ottyajp <ottyajp@gmail.com>
2018-03-02 23:11:45 +0900

    必要と思われるフォームを配置

--- routes/add_sheet.js
+++ routes/add_sheet.js
@@ -5,7 +5,7 @@ const router = express.Router();
 
 router.get('/', (req, res) => {
     const getInst = 'SELECT i_id,i_name,i_group FROM instruments';
-    const getPiece = 'SELECT p_id,title, FROM pieces';
+    const getPiece = 'SELECT p_id,title FROM pieces';
     connection.query(getInst, (instErr, instRows) => {
         connection.query(getPiece, (pieceErr, pieceRows) => {
             res.render('add_sheet', {
