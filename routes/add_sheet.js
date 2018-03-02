1fa870851459343cfccfa94dd2a4057066625f5a
ottyajp <ottyajp@gmail.com>
2018-03-02 22:19:31 +0900

    取り敢えずdroppableな感じで

--- routes/add_sheet.js
+++ routes/add_sheet.js
@@ -4,8 +4,16 @@ const connection = require('../mysqlConnection');
 const router = express.Router();
 
 router.get('/', (req, res) => {
-    res.render('add_sheet', {
-        title:'title'
+    const getInst = 'SELECT i_id,i_name,i_group FROM instruments';
+    const getPiece = 'SELECT p_id,title, FROM pieces';
+    connection.query(getInst, (instErr, instRows) => {
+        connection.query(getPiece, (pieceErr, pieceRows) => {
+            res.render('add_sheet', {
+                title:'title',
+                instruments: instRows,
+                pieces: pieceRows
+            })
+        })
     });
 });
 
