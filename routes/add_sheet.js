7c815a2804334a2ff5487787dffb22a0990cc630
ottyajp <ottyajp@gmail.com>
2018-03-02 23:50:07 +0900

    PDF登録の仕組みを実装

--- routes/add_sheet.js
+++ routes/add_sheet.js
@@ -17,4 +17,15 @@ router.get('/', (req, res) => {
     });
 });
 
+router.post('/', (req, res) => {
+    const p_id = req.body.p_id;
+    const i_id = req.body.i_id;
+    const i_num = req.body.i_num;
+    const path = req.body.path;
+    console.log(p_id, i_id, i_num, path);
+    const query = `INSERT INTO sheets (p_id, i_id, i_num, path) VALUE (${p_id}, ${i_id}, '${i_num}', '${path}')`
+    connection.query(query, (err, rows) => {
+        res.send('ok');
+    });
+});
 module.exports = router;
